937ce54dceae311b94a1da18a5ce323dbe154a3c
David Yang <dmyang@server.fake>
2013-07-24 00:05:05 -0700

    Update all submodules. Reorganized bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,31 +8,66 @@ CONFIGS=$(find $PWD_ABS_PATH/home -maxdepth 1 -not \( -name "$PWD_NAME" -o \
                                                       -name 'bootstrap.sh' -o \
                                                       -name ".git" -o \
                                                       -name ".gitmodules" \))
+JEDI_DIR="$PWD_ABS_PATH/home/vim/bundle/plugin-jedi"
 
-# Initialize all submodules
-(cd "$PWD_ABS_PATH" && git submodule update --init)
+function init() {
+    # Initialize all submodules
+    (cd "$PWD_ABS_PATH" && git submodule update --init)
 
-# Symlink all files
-cd "$HOME"
-for config in $CONFIGS
-do
-    new_config="$HOME/.$(basename $config)"
-    [ -L "$new_config" ] && rm "$new_config"
-    [ -d "$new_config" ] && rm -rI "$new_config"
-    ln -is "$config" $new_config
-done
+    # Initialize Jedi
+    if [ -e "$JEDI_DIR" ]; then
+        (cd "$JEDI_DIR" && git submodule update --init)
+    fi
 
-echo
-echo "
-    Post-install steps:
+    # Symlink all files
+    cd "$HOME"
+    for config in $CONFIGS
+    do
+        new_config="$HOME/.$(basename $config)"
+        [ -L "$new_config" ] && rm "$new_config"
+        [ -d "$new_config" ] && rm -rI "$new_config"
+        ln -is "$config" $new_config
+    done
 
-    Install ctags for your distro. (via apt-get or homebrew)
+    echo
+    echo "
+        Post-install steps:
 
-        apt-get install ctags
+        Install ctags for your distro. (via apt-get or homebrew)
 
-    Initialize the jedi submodule.
+            apt-get install ctags
 
-        cd ~/.vim/bundle/plugins-jedi
-        git submodule update --init
+    "
+}
 
-""
+function update() {
+    # Update all submodules
+    (cd "$PWD_ABS_PATH" && git submodule foreach git pull origin master)
+
+    # Update Jedi
+    if [ -e "$JEDI_DIR" ]; then
+        (cd "$JEDI_DIR" && git submodule foreach git pull origin master)
+    fi
+}
+
+function usage() {
+    echo "Usage: `basename $0` init - intializes all submodules and links files
+                 `basename $0` update - updates all submodules"
+    exit $E_BADARGS
+}
+
+if [ ! "$#" -ne 0 ]; then
+    usage
+fi
+
+case "$1" in
+    init)
+        init
+        ;;
+    update)
+        update
+        ;;
+    *)
+        usage
+        ;;
+esac
