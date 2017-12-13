95ade3c31356f59f8f9a354197545180773b6e4e
David Yang <dyang@pagerduty.com>
2017-12-12 23:19:33 -0800

    Update OSX settings

--- osx/osx.sh
+++ osx/osx.sh
@@ -1,26 +1,49 @@
-# disable Time Machine
-sudo tmutil disablelocal
+#!/usr/bin/env sh
+
+### SYSTEM ###
 
 # un-hide Library folder
 chflags nohidden ~/Library
 
+# disable Time Machine
+sudo tmutil disable
+
 # disable resume
 defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
 
+### KEYBOARD ###
+
 # speed up key repeat
-defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain KeyRepeat -int 2
 
-# disable DS files on network stores
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-
-# disable window animations
-defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+# shorten initial repeat delay
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 # disable press and hold
 defaults write -g ApplePressAndHoldEnabled -bool false
 
+### ANIMATIONS ###
+
+# instant expose
+defaults write com.apple.dock expose-animation-duration -int 0
+defaults write com.apple.dock springboard-show-duration -int 0
+defaults write com.apple.dock springboard-hide-duration -int 0
+
+# disable window animations
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+
+### DOCK ###
+
 # enable spring load on dock
 defaults write enable-spring-load-actions-on-all-items -bool true
+
+# scale effect
+defaults write com.apple.dock mineffect -string scale
+
+# disable autocorrect
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+### DIALOGS ###
 
 # enable all keys in dialogs
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
@@ -31,24 +54,6 @@ defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
 # expand print dialogs
 defaults write -g PMPrintingExpandedStateForPrint -bool true
 
-# enable 3D dock
-defaults write com.apple.dock no-glass -bool false
-
-# disable autocorrect
-defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-
-# disable window animation
-defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool NO
-
-# instant expose
-defaults write com.apple.dock expose-animation-duration -int 0
-defaults write com.apple.dock springboard-show-duration -int 0
-defaults write com.apple.dock springboard-hide-duration -int 0
-
-# suck effect
-defaults write com.apple.dock mineffect -string suck
+### RESET DOCK ###
 
 killall Dock
-
-# Disable autostart of xterm in XQuartz
-defaults write org.x.X11 app_to_run /usr/bin/true
